1fe437c2175a09f05023bbea9f5750f936c52c60
tejpalDarro <tejpaldarro007@gmail.com>
2024-05-23 09:06:02 +0530

    day 2

--- Display.java
+++ Display.java
@@ -2,19 +2,51 @@ import java.awt.Canvas;
 
 import javax.swing.JFrame;
 
-public class Display extends Canvas{
+public class Display extends Canvas implements Runnable{
     public static int HEIGHT = 800;
     public static int WIDTH = 600;
     public static final String TITLE = "JframeGame";
     
+    private Thread thread;
+    private boolean running = false;
+
+    private void start() {
+        if (running) return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+        System.out.println("start");
+    }
+
+    private void stop() {
+        if (!running) return;
+        running = false;
+        try {
+            thread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        System.out.println("stop");
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+        }
+    }
+
     public static void main(String[] args) {
         Display game = new Display();
         JFrame frame = new JFrame();
         frame.add(game);
         frame.setTitle(TITLE);
+        // close frame when exit
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(WIDTH, HEIGHT);
         frame.setResizable(true);
         frame.setVisible(true);
+        game.start();
     }
+
 }
